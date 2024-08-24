38e23e5a9776c79a7b185ca46f5a71394455ccd0
Thomas Glasser <tjglass06@gmail.com>
2024-08-24 15:12:10 -0400

    Snapshot version

--- src/lib/components/header/HeaderBar.svelte
+++ src/lib/components/header/HeaderBar.svelte
@@ -34,7 +34,7 @@
 			<p
 				class="text-sm sm:text-lg md:text-3xl font-bold uppercase mr-4"
 				id="version"
-				title="Return to main page">Mod Version: 0.0.0
+				title="Return to main page">Mod Version: 00e01a
 			</p>
 		</div>
 		<div class="hidden lg:block">
