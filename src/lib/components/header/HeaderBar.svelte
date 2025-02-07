3477cf6170009497c74fabe22a30a05ac949f556
Thomas Glasser <thomas@thomasglasser.dev>
2025-02-07 16:47:11 -0500

    Fixed mod version Added miraculous.png Added repeating_command_block.png

--- src/lib/components/header/HeaderBar.svelte
+++ src/lib/components/header/HeaderBar.svelte
@@ -34,7 +34,7 @@
 			<p
 				class="text-sm sm:text-lg md:text-3xl font-bold uppercase mr-4"
 				id="version"
-				title="Return to main page">Mod Version: N/A
+				title="Return to main page">Mod Version: 01e01a
 			</p>
 		</div>
 		<div class="hidden lg:block">
