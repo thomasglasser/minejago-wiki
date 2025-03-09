89d2a096bc8ee9d432ea5ca4e6fdc6186dc16b42
Thomas Glasser <thomas@thomasglasser.dev>
2025-03-09 04:40:14 -0400

    Updated mod version

--- src/lib/components/header/HeaderBar.svelte
+++ src/lib/components/header/HeaderBar.svelte
@@ -34,7 +34,7 @@
 			<p
 				class="text-sm sm:text-lg md:text-3xl font-bold uppercase mr-4"
 				id="version"
-				title="Return to main page">Mod Version: 01e01b
+				title="Return to main page">Mod Version: 01e01f
 			</p>
 		</div>
 		<div class="hidden lg:block">
