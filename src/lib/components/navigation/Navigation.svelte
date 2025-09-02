4065a248cc0414ae4575d99d8de9e0809f8f4e43
Thomas Glasser <thomas@thomasglasser.dev>
2025-09-02 00:37:25 -0400

    Updated wiki for 1.0.0-beta1

--- src/lib/components/navigation/Navigation.svelte
+++ src/lib/components/navigation/Navigation.svelte
@@ -1,6 +1,8 @@
 <script lang="ts" context="module">
 	import { getItemSrc } from '$lib/textures.js';
 	import { notDisplayableItems } from '$lib/components/modonomicon/RecipeDisplay/notDisplayableItems';
+	import { getTexture } from '$lib/textures';
+	import { getFileName } from '$lib/utils/fileName';
 
 	const checkNavIcon = (iconSrc: string, textureStore: App.TextureDictionary) => {
 		if (notDisplayableItems.includes(iconSrc)) {
